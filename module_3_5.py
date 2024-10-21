968f83d6b27ee876f8f07549ae4de7040eaa859e
Vasugan11 <triapolitanets@gmail.com>
2024-10-21 12:49:40 +0400

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,12 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    str_number = str_number.replace('0', '')
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return    first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+rez = get_multiplied_digits(42057)
+
+print(rez)
